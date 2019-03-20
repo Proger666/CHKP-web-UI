8c752539cc417bc9054ba7d2de17e0e0f0406baf
Ilya <ketroon@gmail.com>
2019-03-20 13:18:33 +0100

    Test

--- scripts/rest_calls.py
+++ scripts/rest_calls.py
@@ -2,11 +2,12 @@ import basic_calls as bc
 import chkp_parsers as pars
 
 def show_gateways(mgmt_json):
-    #management_data = [mgmt_json['mgmt_ip'], mgmt_json[mgmt]]
-    return pars.rest_get_all_gateways(bc.get_all_gateways(mgmt_json))
+    management_data = [mgmt_json['mgmt_ip'], mgmt_json['mgmt_port'], mgmt_json['mgmt-sid']]
+    return pars.rest_get_all_gateways(bc.get_all_gateways(management_data))
 
-def get_sid(mgmt_data, username, password):
-    return bc.login(mgmt_data, username, password)
+def get_sid(mgmt_json, username, password):
+    management_data = [mgmt_json['mgmt_ip'], mgmt_json['mgmt_port'], {}]
+    return bc.login(management_data, username, password)
 
 def main():
     management_ip = '172.16.198.129'
