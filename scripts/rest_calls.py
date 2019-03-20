727f4090f826a7ea5b996c6071f7b1620f40bd02
Ilya <ketroon@gmail.com>
2019-03-20 15:04:45 +0100

    Test

--- scripts/rest_calls.py
+++ scripts/rest_calls.py
@@ -1,14 +1,19 @@
 import basic_calls as bc
 import chkp_parsers as pars
+import json
+
 
 def show_gateways(mgmt_json):
     management_data = [mgmt_json['mgmt_ip'], mgmt_json['mgmt_port'], mgmt_json['mgmt-sid']]
-    return pars.rest_get_all_gateways(bc.get_all_gateways(management_data))
+    return json.dumps(pars.rest_get_all_gateways(bc.get_all_gateways(management_data)))
+
 
 def get_sid(mgmt_json, username, password):
     management_data = [mgmt_json['mgmt_ip'], mgmt_json['mgmt_port'], {}]
-    return bc.login(management_data, username, password)
+    return json.dumps(bc.login(management_data, username, password))
 
+
+'''
 def main():
     management_ip = '172.16.198.129'
     management_port = '443'
@@ -16,10 +21,11 @@ def main():
     username = 'admin'
     secret = '123qweASD'
     management_data = [management_ip, management_port, sid]
-    sid = get_sid(management_data, username, secret)
-    management_data[2] = sid
+    sid = get_sid_test(management_data, username, secret)
+    print(sid)
+    management_data[2] = sid[1:-1]
 
-    gws = show_gateways(management_data)
+    gws = json.loads(show_gateways_test(management_data))
     bc.logout(management_data)
     print(gws)
     return 0
@@ -27,3 +33,4 @@ def main():
 
 if __name__ == '__main__':
   main()
+'''
